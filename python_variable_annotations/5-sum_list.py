341194fec56b521f134837c145a497d4632fffa9
Kamalusta <kamalusta@gmail.com>
2024-05-01 23:47:39 +0400

    5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""take list and return sum as float"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """take list and return sum as float"""
+    return sum(input_list)
