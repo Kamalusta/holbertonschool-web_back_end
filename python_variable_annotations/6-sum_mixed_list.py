07ca2b40fe81fb984330f0d0574be3cb496277ec
Kamalusta <kamalusta@gmail.com>
2024-05-02 11:25:16 +0400

    6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""takes int and float to sum of float"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """takes int and float to sum of float"""
+    return sum(mxd_lst)
+    
