9bb194a72bfb28119a3b9a4174e5c4d731054c3a
Kamalusta <kamalusta@gmail.com>
2024-05-03 12:42:54 +0400

    0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,8 @@ from random import uniform
 import asyncio
 
 
-async def wait_random(max_delay: int = 10):
-    sec: float = uniform(0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    """doc"""
+    sec = uniform(0, max_delay)
     await asyncio.sleep(sec)
     return sec
