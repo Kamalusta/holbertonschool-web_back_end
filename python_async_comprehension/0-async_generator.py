ff82049845bb86ca3c507efdf54e24c60c71d4ce
Kamalusta <kamalusta@gmail.com>
2024-05-04 16:11:39 +0400

    1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-""" dioc"""
+"""yield a random number"""
 
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    """doc"""
+async def async_generator() -> Generator[float, None, None]:
+    """yield a random number"""
     for _ in range(10):
         await asyncio.sleep(1)
         nbr = random.uniform(0, 10)
