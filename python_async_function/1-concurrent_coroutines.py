87004d023d1d5db604da136cc066301000b530b7
Kamalusta <kamalusta@gmail.com>
2024-05-04 10:01:56 +0400

    1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,24 @@
 """doc"""
 
 
-from 0-basic_async_syntax import wait_random
+from basic_async_syntax import wait_random
 import asyncio
+from typing import List
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """doc"""
-    
+    """Returns a list of floats representing random delays.
+    delays = []
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+
+    for future in asyncio.as_completed(coroutines):
+        delay = await future
+        delays.append(delay)
+
+    return delays"""
+
+    delays = []
+    for _ in range(n):
+        delays.append(wait_random(max_delay))
+    results = await asyncio.gather(*delays)
+    return sorted(results)
