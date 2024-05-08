376847470e2bc7ab969c90542d907215eb0d29c1
Kamalusta <kamalusta@gmail.com>
2024-05-09 02:01:44 +0400

    corrections

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env pytho3
 """ lists all documents"""
 
-import pymongo
+
 from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """ lists all documents"""
+    if mongo_collection is None:
+        return []
     return mongo_collection.find()
