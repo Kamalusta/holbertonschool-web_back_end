b21b559b36d4e5996f18592101acf3e233e603a4
Kamalusta <kamalusta@gmail.com>
2024-05-11 11:42:11 +0400

    0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""doc"""
+"""pagination start end"""
 
 
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """doc"""
+    """pagination start end"""
     start = (page - 1) * page_size
     end = start + page_size
     return start, end
