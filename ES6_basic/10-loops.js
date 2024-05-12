5829248886f1e08a7442778c65ea8b2185a1bdde
Kamalusta <kamalusta@gmail.com>
2024-05-12 22:31:19 +0400

    10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
     const newArray = [];
     for (const idx of array) {
-        let value = idx;
-        newArray.push(appendString + value);
+        newArray.push(appendString + idx);
     }
 
-    return array;
+    return newArray;
 }
