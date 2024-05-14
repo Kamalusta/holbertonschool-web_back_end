4f50c290d832565c0073a56b471bb74401a1fbe4
Kamalusta <kamalusta@gmail.com>
2024-05-15 01:27:46 +0400

    10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+import { threadId } from "worker_threads";
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
