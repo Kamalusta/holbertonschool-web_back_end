c3927e54ea4dfdcd8e0314293f3dd15310bbc30d
Kamalusta <kamalusta@gmail.com>
2024-05-14 16:57:14 +0400

    5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+import { error } from "console";
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
