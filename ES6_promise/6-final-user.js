17b08b97675e82968108fac75434a470511ff1ce
Kamalusta <kamalusta@gmail.com>
2024-05-23 00:40:29 +0400

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,11 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((data) => {
-      return {
-        status: Promise.status,
-        value: data,
-      }
-    })
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => (
+      data.map((dat) => ({
+        status: dat.status,
+        value: dat.value ? dat.value : String(dat.reason),
+      }))
+    ));
 }
