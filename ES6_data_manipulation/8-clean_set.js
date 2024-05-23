929c2bd6f52fc7196d96a0a61fd54bf8d2f31e1a
Kamalusta <kamalusta@gmail.com>
2024-05-24 03:39:36 +0400

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,6 @@ export default function cleanSet(set, startString) {
   if (!startString || !startString.length) return '';
   const newArr = Array.from(set)
     .filter((values) => values.startsWith(startString));
-  const newarr = newArr.map((value) => value.replace(startString, ''))
+  const newarr = newArr.map((value) => value.replace(startString, ''));
   return newarr.join('-');
 }
