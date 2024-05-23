72acaf67b65608dcbb2a0126d57c85e5374459a3
Kamalusta <kamalusta@gmail.com>
2024-05-24 03:50:50 +0400

    9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const mymap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 12],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return mymap;
+};
